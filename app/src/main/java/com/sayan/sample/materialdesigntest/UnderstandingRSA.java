be5f6eeae628867d403596a97ff8a65b47a2a36c
sayan <anshu.p@encoders.co.in>
2018-10-05 18:20:08 +0530

    step 5 of https://www.twilio.com/blog/what-is-public-key-cryptography done

--- app/src/main/java/com/sayan/sample/materialdesigntest/UnderstandingRSA.java
+++ app/src/main/java/com/sayan/sample/materialdesigntest/UnderstandingRSA.java
@@ -18,11 +18,20 @@ public class UnderstandingRSA extends AppCompatActivity {
         int j = PrimeNumberGenerator.generateNextPrime(i);
         long multi = i * j;
         long lcm = LCMGenerator.findLCM(i - 1, j - 1);
-
-        NumberNotDivisorPicker.pick(12);
+        long notDivisor = NumberNotDivisorPicker.pick(lcm);
+//        inverseMOD(notDivisor, lcm);
+        int inverseMOD = inverseMOD(23, 2310);
 
         Toast.makeText(this, "" + i, Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "" + j, Toast.LENGTH_SHORT).show();
+    }
+
+    private int inverseMOD(long notDivisor, long lcm) {
+        notDivisor = notDivisor % lcm;
+        for (int x = 1; x < lcm; x++)
+            if ((notDivisor * x) % lcm == 1)
+                return x;
+        return 1;
     }
 
     static class PrimeNumberGenerator {
@@ -32,10 +41,10 @@ public class UnderstandingRSA extends AppCompatActivity {
         static int generatePrime() {
             int num = 0;
             final Random rand = new Random(); // generate a random number
-            num = rand.nextInt( MAX_BOUND + 1 - MIN_BOUND) + MIN_BOUND;
+            num = rand.nextInt(MAX_BOUND + 1 - MIN_BOUND) + MIN_BOUND;
 
             while (!isPrime(num)) {
-                num = rand.nextInt( MAX_BOUND + 1 - MIN_BOUND) + MIN_BOUND;
+                num = rand.nextInt(MAX_BOUND + 1 - MIN_BOUND) + MIN_BOUND;
             }
             return num;
         }
@@ -51,7 +60,7 @@ public class UnderstandingRSA extends AppCompatActivity {
         /**
          * Checks to see if the requested value is prime.
          */
-        static boolean isPrime(int inputNum){
+        static boolean isPrime(int inputNum) {
             if (inputNum <= 3 || inputNum % 2 == 0)
                 return inputNum == 2 || inputNum == 3; //this returns false if number is <=1 & true if number = 2 or 3
             int divisor = 3;
@@ -62,23 +71,23 @@ public class UnderstandingRSA extends AppCompatActivity {
     }
 
     static class LCMGenerator {
-        static long findLCM(int a, int b){
-            return (a * b) / findGCD(a,b);
+        static long findLCM(int a, int b) {
+            return (a * b) / findGCD(a, b);
         }
 
         static long findGCD(int a, int b) {
-            if (b==0) return a;
-            return findGCD(b,a%b);
+            if (b == 0) return a;
+            return findGCD(b, a % b);
         }
     }
 
     static class NumberNotDivisorPicker {
-        static long pick(long number){
+        static long pick(long number) {
             final Random rand = new Random(); // generate a random number
             //generate a randomNumber between half of the number and the number
-            long randomNumber = rand.nextInt((int) (number/2)) + (int)number/2;
+            long randomNumber = rand.nextInt((int) (number / 2)) + (int) number / 2;
             //check if randomNumber is a divisor of the number
-            while ((number % randomNumber) == 0){
+            while ((number % randomNumber) == 0) {
                 randomNumber++;
             }
             return randomNumber;
